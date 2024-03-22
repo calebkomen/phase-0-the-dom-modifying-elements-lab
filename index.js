316da23a0574141dc831f6e817fe6177131f8079
CALEB KOMEN <komencaleb502@gmail.com>
2024-03-22 16:14:47 +0300

    het

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const newHeader = document.querySelector('h1');
+
+document.addEventListener('DOMContentLoaded', () => {
+    var newHeader = document.getElementById('victory');
+    newHeader.textContent = 'KOMEN-CALEB is the champion';
+});
